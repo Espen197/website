05698ac0647976e0d5d18124eed3dab4a18df89a
Espen Høijord <espehaho@gmail.com>
2019-02-09 18:00:36 +0100

    Small changes to models

--- stocks/models.py
+++ stocks/models.py
@@ -11,12 +11,10 @@ class Portfolio(models.Model):
 class Stock(models.Model):
     portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
     ticker = models.CharField(max_length=5)
-    price = models.DecimalField(max_digits=10, decimal_places=3)
-    # opening = models.DecimalField(max_digits=10, decimal_places=3)
-    # close = models.DecimalField(max_digits=10, decimal_places=3)
-    volume = models.DecimalField(max_digits=20, decimal_places=3)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    volume = models.IntegerField()
 
     def __str__(self):
-        return self.ticker + ": " + self.price
+        return self.ticker + ": " + str(self.volume) + " for " + str(self.price) + " NOK per stykk."
 
 
